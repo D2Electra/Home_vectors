5a364dad9b4e86be02c4726dbd936330d96814b3
D2Electra <kalkidik1@yandex.ru>
2023-04-16 12:48:53 +0300

    v3

--- Home_vectors/__main__.py
+++ Home_vectors/__main__.py
@@ -2,28 +2,39 @@
 import py5
 import random
 import vect_point
+import vect_line
 
 def setup():
-    global delta_x, delta_y, x_coord, y_coord, my_point, another_point
+    global delta_x, delta_y, x_coord, y_coord, my_point, another_point,one_point,red_line
     py5.size(500, 500)
     py5.background(0)
     #py5.stroke(255, 0, 0)
-    py5.fill(255, 0, 0)
+    py5.fill(255, 211, 0)
     delta_x = 2
     delta_y = 3
     x_coord = 60
     y_coord = 60
     my_point = vect_point.Point(50, 50)
     another_point = vect_point.Point(70, 150)
+    one_point = vect_point.Point(20, 100)
+    red_line = vect_line.Line(100,200)
+    
     #py5.no_loop()
 
 def draw():
-    global delta_x, delta_y, x_coord, y_coord, my_point, another_point
+    global delta_x, delta_y, x_coord, y_coord, my_point, another_point,one_point,my_line
     py5.background(0)
     my_point.move()
     another_point.move()
+    one_point.move()
+    red_line.move()
+    my_point.check_edges()
+    another_point.check_edges()
+    one_point.check_edges()
     my_point.display()
     another_point.display()
-
+    one_point.display()
+    red_line.display()
+    
 
 py5.run_sketch()
--- Home_vectors/vect_line.py
+++ Home_vectors/vect_line.py
@@ -1,40 +1,43 @@
-# -*- coding: utf-8 -*-
 import py5
 import random
 
-class Point (object):
+class Line (object):
     def __init__(self, xpos, ypos):
-        self.position = py5.Py5Vector(xpos, ypos)
+        self.position = py5.Py5Vector(xpos, ypos )#у вектора две координаты!!
         self.velocity = py5.Py5Vector(py5.random(0.9), py5.random(0.9))
         self.acceleration = py5.Py5Vector(py5.random(0.01),py5.random(0.03))
         #self.diameter = random.randint(5, 50)
-        self.c = py5.color(255, 211, 0)
+        self.c = py5.color(255, 0, 0)
         
     def move(self):
         self.velocity += self.acceleration
         self.position += self.velocity
+        
+        
         
     
     def display(self):
         py5.push_matrix()
         py5.translate(self.position.x, self.position.y)
         py5.stroke(self.c)
-        py5.stroke_weight(5)
-        py5.point(0, 0)
-        py5.pop_matrix()
-
+        py5.stroke_weight(0.5)
+        py5.line(0, 0, 0, 50)
+        py5.line(0, 0, 0, -50)
+        py5.pop_matrix()# -*- coding: utf-8 -*-
+    
+    # не понимаю как это записать
     def check_edges(self):
         if self.position.x > py5.width:
             self.velocity.x = -self.velocity.x
-            self.position.x = py5.width 
+            self.position.x = 
         if self.position.x < 0:
             self.velocity.x = -self.velocity.x
-            self.position.x = self.position.x
+            self.position.x = 0
         if self.position.y > py5.height:
             self.velocity.y = -self.velocity.y
-            self.position.y = py5.height
+            self.position.y = 
         if self.position.y < 0:
             self.velocity.y = -self.velocity.y
-            self.position.y = self.position.y
+            self.position.y = 
+     
         
-            
